0d0e116e99d943ee2f2b06e517b28d0ca776f45c
xtzeng <xiaoti_zeng@126.com>
2014-07-15 17:29:40 +0800

    add session.setMaxInactiveInterval(60)

--- src/main/java/com/immutable/alias/servlet/EncodeURL.java
+++ src/main/java/com/immutable/alias/servlet/EncodeURL.java
@@ -8,12 +8,17 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class EncodeURL extends HttpServlet{
 
 	 /**
 	 */
 	private static final long serialVersionUID = 1L;
 
+	private Logger logger = LoggerFactory.getLogger(this.getClass());
+	
 	public void doGet(HttpServletRequest request, HttpServletResponse response)  
      throws ServletException, IOException {
 		 
@@ -21,6 +26,11 @@ public class EncodeURL extends HttpServlet{
 		 response.setContentType("text/html;charset=utf-8");  
 		 PrintWriter out = response.getWriter();  
 		 request.getSession();  //创建session  
+		 
+		 request.getSession().setMaxInactiveInterval(60);
+		 
+		 logger.info("SessionId:========" + request.getSession().getId());
+		 
 		 //调用response的encodeURL方法，将自动将JSESSION追加到url后面，如：url;jsessionid=BD111FFC653497E81B702A29B3AC6FE4  
 		 String buyurl = response.encodeURL("/urlencode");  
 		 String payurl = response.encodeURL("/urlencode");  
